2ed37c300b17058b14f1bce72ed502a9d91cb9b1
DanXuuuuu <139434572+DanXuuuuu@users.noreply.github.com>
2026-01-18 14:28:52 -0800

    Update feat: password validation and hash password with bcrypt

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-
+    username:{
+        type: String,
+        required: true,
+        trm: true,
+        unique: true,
+        minlength: 3,
+    },
     email:{
         type: String,
         required: true,
@@ -16,14 +23,24 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 6,
-        // not finish yet cuz validate not write yet 
+        // password format validate
         validate:{
             validator: function(password){
-             
+                // check if validate password format is fit
+             if(this.isModified('password') && !password.startsWith('$2a$')){
+                const hasNumber = /\d/.test(password);
+                const hasSymbol =  /[!@#$%^&*(),.?":{}|<>]/.test(password);
+                const hasUppercase =  /[A-Z]/.test(password);
+                const hasLowercase = /[a-z]/.test(password);
+
+                return hasNumber && hasSymbol && hasUppercase && hasLowercase;
+             }
+             return true;
 
             },
             message: 'Invalid password input!'
         }
+
 
     },
     createdAt:{
@@ -31,8 +48,22 @@ const userSchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+
+userSchema.pre('save', async function(next){
+    // check if we dont need encrypt cuz the password is not change
+    if(!this.isModified('password')) return next();
+    
+    // the password change 
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+// this is verify for check the content of login
+userSchema.methods.comparePassword = async function(candidatePassword){
+    return await bcrypt.compare(candidatePassword, this.password);
+
+};
+
 // model makes schema could be operate 
 const User = mongoose.model('User', userSchema);
-
-
 module.exports = User;
