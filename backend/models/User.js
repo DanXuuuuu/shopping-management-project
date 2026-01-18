8b7ac7ccd5a5757d9fc6cd5dad4c0a7ae6c54fb2
DanXuuuuu <139434572+DanXuuuuu@users.noreply.github.com>
2026-01-17 23:58:37 -0800

    validate email format

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,21 +1,38 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
 
-//     email:{
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     password:{
-//         type: String,
-//         required: true,
-//         minilength: 6
-//         // not finish yet cuz validate
-//     }
-// });
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        // validate email format 
+        match:[/^\S+@\S+\.\S+$/, 'Invalid Email input!']
 
-// const User = mongoose.model('User', userSchema);
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 6,
+        // not finish yet cuz validate not write yet 
+        validate:{
+            validator: function(password){
+             
+
+            },
+            message: 'Invalid password input!'
+        }
+
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+// model makes schema could be operate 
+const User = mongoose.model('User', userSchema);
 
 
-// module.exports = User;
+module.exports = User;
